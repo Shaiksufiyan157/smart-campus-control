c231e98b26a6e1339b9d8e725de9cbb9e43962e6
sufiyan <shaiksufiyan157@gmail.com.com>
2025-07-09 19:04:19 +0530

    added contribution route

--- backend/src/controllers/resources.controllers.js
+++ backend/src/controllers/resources.controllers.js
@@ -7,8 +7,12 @@ res.render('resources/pyq.ejs')
 const rendernotes=(req,res)=>{
 res.render("resources/notes.ejs")
 }
+const renderAddnotes=(req,res)=>{
+res.render("contribution/notes.ejs")
+}
 const resourcecontroller={
 renderpyq,
-rendernotes
+rendernotes,
+renderAddnotes
 }
 export default resourcecontroller;
--- backend/src/routes/resources.route.js
+++ backend/src/routes/resources.route.js
@@ -1,10 +1,17 @@
 import express from "express"
 import resourcecontroller from "../controllers/resources.controllers.js";
-
+import multer from 'multer'
+const upload = multer({ dest: 'uploads/' })
 const router=express.Router()
 
-router.get('/pyq',resourcecontroller.renderpyq);
 
+
+router.get('/pyq',resourcecontroller.renderpyq);
 router.get('/notes',resourcecontroller.rendernotes);
 
+router.route('/addnotes')
+.get(resourcecontroller.renderAddnotes)
+.post(upload.array('notes'),(req,res)=>{
+res.send(req.body,req.files);
+});
 export default router;
