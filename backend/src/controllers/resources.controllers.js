c3168a9a34dc834d8d714fbf8125185bac308fbf
sufiyan <shaiksufiyan157@gmail.com.com>
2025-07-11 21:25:35 +0530

    added title to notes

--- backend/src/controllers/resources.controllers.js
+++ backend/src/controllers/resources.controllers.js
@@ -1,36 +1,36 @@
 import Note from "../models/notes.model.js"
 
-const renderpyq=(req,res)=>{
-res.render('resources/pyq.ejs')
+const renderpyq = (req, res) => {
+    res.render('resources/pyq.ejs')
 }
-const rendernotes=  async (req,res)=>{
-const notes= await Note.find({});
-console.log(notes)
-res.render("resources/notes.ejs",{notes})
+const rendernotes = async (req, res) => {
+    const notes = await Note.find({});
+    console.log(notes)
+    res.render("resources/notes.ejs", { notes })
 }
-const renderAddnotes=(req,res)=>{
-res.render("contribution/notes.ejs")
+const renderAddnotes = (req, res) => {
+    res.render("contribution/notes.ejs")
 }
-const renderAddpyqs=(req,res)=>{
-res.render("contribution/pyq.ejs")
+const renderAddpyqs = (req, res) => {
+    res.render("contribution/pyq.ejs")
 }
-const Addnotes=async(req,res)=>{
-const Notes=new Note(req.body);
-Notes.name=req.body.name;
-Notes.images=req.files.map(f=>({url:f.path,filename:f.filename }))
-await Notes.save();
-console.log(Notes);
-res.redirect('/home')
+const Addnotes = async (req, res) => {
+    const Notes = new Note(req.body);
+    // Notes.name=req.body.;
+    Notes.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    await Notes.save();
+    console.log(Notes);
+    res.redirect('/home')
 }
-const Addpyqs=(req,res)=>{
-res.send(req.files)
+const Addpyqs = (req, res) => {
+    res.send(req.files)
 }
-const resourcecontroller={
-renderpyq,
-rendernotes,
-renderAddnotes,
-renderAddpyqs,
-Addnotes,
-Addpyqs
+const resourcecontroller = {
+    renderpyq,
+    rendernotes,
+    renderAddnotes,
+    renderAddpyqs,
+    Addnotes,
+    Addpyqs
 }
 export default resourcecontroller;
