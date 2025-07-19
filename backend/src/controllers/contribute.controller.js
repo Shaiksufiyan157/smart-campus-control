bbb7786d4caed662a61f94d0289d02706775efbd
sufiyan <shaiksufiyan157@gmail.com.com>
2025-07-19 19:29:28 +0530

    issue with pdf files

--- backend/src/controllers/contribute.controller.js
+++ backend/src/controllers/contribute.controller.js
@@ -9,27 +9,38 @@ const renderAddpyqs = (req, res) => {
     res.render("contribution/pyq.ejs")
 }
 const Addnotes = async (req, res) => {
-    const content = req.body;
-    const Notes = new Note(content);
+    // const content = req.body;
+    const Notes = new Note(req.body);
     // const customName = content.subject + "(" + content.subcode + ")" || 'smart campus control';
-console.log("working")
+    console.log("working")
+console.log(req.file)
     // const uploadedImage = await cloudinary.uploader.upload(req.file.path, {
     //     public_id: customName,
     //     overwrite: true
     // });
-    const originalName = path.parse(req.file.originalname).name;
-    console.log(originalName)
-    const uploadedImage = await cloudinary.uploader.upload(req.file.path, {
-        public_id: originalName,
-        overwrite: true,
-    });
-    Notes.image = {
-        url: uploadedImage.secure_url,
-        filename: uploadedImage.public_id,
-    };
+// console.log(req.file)
+    if (req.file.mimetype === "image/jpeg" || req.file.mimetype === "image/png" || req.file.mimetype === "image/jpg") {
+console.log("from if")
+        const originalName = path.parse(req.file.originalname).name;
+        console.log(originalName)
+        const uploadedImage = await cloudinary.uploader.upload(req.file.path, {
+            public_id: originalName,
+            overwrite: true,
+        });
+        Notes.image = {
+            url: uploadedImage.secure_url,
+            filename: uploadedImage.public_id,
+        };
+
+    }
+    else {
+        const image = req.file;
+        Notes.image = { url: image.path, filename: image.filename };
+    }
+
+
 
     // Notes.images = req.files.map(f => ({ url: f.path, filename: f.filename }))--------------------> to upload many images
-    // Notes.image={url:image.path,filename:image.filename};
 
     console.log(Notes);
     await Notes.save();
